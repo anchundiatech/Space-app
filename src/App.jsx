3fe4280742e3933d79a173e701ddc60d3a4d9d8a
Alejodev <alejodev2022@gmail.com>
2024-07-13 13:27:44 -0500

    Finalizacion del curso

---
+++ src/App.jsx
@@ -0,0 +1,58 @@
+import styled from "styled-components"
+import GlobalStyles from "./components/GlobalStyles"
+import Cabecera from "./components/Cabecera"
+import BarraLateral from "./components/BarraLateral"
+import Banner from "./components/Banner"
+import banner from "./assets/banner.png"
+import Galeria from "./components/Galeria"
+import fotos from "./fotos.json"
+import { useState } from "react"
+import ModalZoom from "./components/ModalZoom"
+
+const FondoGradiente = styled.div`
+background: linear-gradient(175deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
+width:100%;
+min-height:100vh;
+`
+const AppContainer = styled.div`
+  width:1280px;
+  max-width:100%;
+margin: 0 auto;
+`
+const MainContainer = styled.main`
+  display: flex;
+  gap:24px;
+`
+const ContenidoGaleria = styled.section`
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+`
+
+
+const App = ()=> {
+const [fotosDeGaleria, setFotosDeGaleria]= useState(fotos)
+const [fotoSeleccionada,setFotoSeleccionada]= useState(null)
+
+  return (
+    <>
+      <FondoGradiente>
+        <GlobalStyles />
+        <AppContainer>
+          <Cabecera />
+          <MainContainer>
+            <BarraLateral />
+            <ContenidoGaleria>
+              <Banner texto="La galería más completa de fotos del espacio" backgroundImage={banner} />
+
+              <Galeria alSeleccionarFoto={foto=>setFotoSeleccionada(foto)} fotos={fotosDeGaleria}/>
+            </ContenidoGaleria>
+          </MainContainer>
+        </AppContainer>
+        <ModalZoom foto={fotoSeleccionada}/>
+      </FondoGradiente>
+    </>
+  )
+}
+
+export default App
